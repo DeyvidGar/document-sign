a51fd990f00a57debf1d681b75d8abf28d580018
David G <davidgar8879@gmail.com>
2025-08-24 15:49:48 -0600

    Update Exchange constants file

--- src/main/java/com/midominio/camel/documentsign/constants/ExchangeConstants.java
+++ src/main/java/com/midominio/camel/documentsign/constants/ExchangeConstants.java
@@ -1,17 +1,21 @@
 package com.midominio.camel.documentsign.constants;
 
 /** Class for constants values. */
-public class AppConstants {
+public class ExchangeConstants {
 
     /**
      * Headers.
      */
+    /** Header value 'documentId'. */
     public static String DOCUMENT_ID = "documentId";
+    /** Header value 'ownerId'. */
     public static String OWNER_ID = "ownerId";
+    /** Header value 'documentType'. */
     public static String DOCUMENT_TYPE = "documentType";
+    /** Header value 'clientId'. */
     public static String CLIENT_ID = "clientId";
 
     /** Private Constructor for not instance. */
-    private AppConstants() {
+    private ExchangeConstants() {
     }
 }
